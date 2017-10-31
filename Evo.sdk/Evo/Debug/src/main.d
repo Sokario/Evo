bef863f994c33c75dc8ed5529b322d734f4f22fb
Logan SAINT GERMAIN <logansaintgermain@gmail.com>
2017-10-31 14:07:23 +0100

    Working getting started from zynq book

--- Evo.sdk/Evo/Debug/src/main.d
+++ Evo.sdk/Evo/Debug/src/main.d
@@ -3,7 +3,17 @@ src/main.o src/main.o: ../src/main.c ../src/platform.h \
  ../../Evo_bsp/ps7_cortexa9_0/include/xil_printf.h \
  ../../Evo_bsp/ps7_cortexa9_0/include/xil_types.h \
  ../../Evo_bsp/ps7_cortexa9_0/include/xparameters.h \
- ../../Evo_bsp/ps7_cortexa9_0/include/xparameters_ps.h
+ ../../Evo_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Evo_bsp/ps7_cortexa9_0/include/sleep.h \
+ ../../Evo_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../Evo_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Evo_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../Evo_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../Evo_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../Evo_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../Evo_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../Evo_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../Evo_bsp/ps7_cortexa9_0/include/xgpio_l.h
 
 ../src/platform.h:
 
@@ -16,3 +26,23 @@ src/main.o src/main.o: ../src/main.c ../src/platform.h \
 ../../Evo_bsp/ps7_cortexa9_0/include/xparameters.h:
 
 ../../Evo_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Evo_bsp/ps7_cortexa9_0/include/sleep.h:
+
+../../Evo_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../Evo_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Evo_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../Evo_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../Evo_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../Evo_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../Evo_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../Evo_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../Evo_bsp/ps7_cortexa9_0/include/xgpio_l.h:
